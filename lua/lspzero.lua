9d296d98cc650aa3fe9978912e67491f509567f0
Zorex Salvo <zorexsalvo@gmail.com>
2024-12-06 08:00:42 +0800

    Update lspzero config

--- lua/lspzero.lua
+++ lua/lspzero.lua
@@ -1,24 +1,37 @@
-local lsp_zero = require('lsp-zero')
+local lsp = require('lsp-zero').preset({
+    manage_nvim_cmp = true, -- Enable cmp management by lsp-zero
+    set_lsp_keymaps = true, -- Default keymaps
+    suggest_lsp_servers = true, -- Automatically suggest servers
+})
 
-lsp_zero.on_attach(function(client, bufnr)
-  -- see :help lsp-zero-keybindings
-  -- to learn the available actions
-  lsp_zero.default_keymaps({buffer = bufnr})
+-- Ensure Python LSP is installed
+lsp.ensure_installed({
+    'pyright', -- or pylsp, depending on your preference
+})
+
+-- Configure CMP for Autocompletion
+local cmp = require('cmp')
+local cmp_select = {behavior = cmp.SelectBehavior.Select}
+local cmp_mappings = lsp.defaults.cmp_mappings({
+    ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+    ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+    ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+    ['<C-Space>'] = cmp.mapping.complete(),
+})
+
+lsp.setup_nvim_cmp({
+    mapping = cmp_mappings
+})
+
+-- On Attach Callback for Additional Configurations
+lsp.on_attach(function(client, bufnr)
+    local opts = {buffer = bufnr, remap = false}
+    vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+    vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+    vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+    vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
 end)
 
-require("mason").setup()
-require("mason-lspconfig").setup()
-
--- After setting up mason-lspconfig you may set up servers via lspconfig
-require("lspconfig").pyright.setup {}
--- require("lspconfig").rust_analyzer.setup {}
---
- require("lspconfig").ruff_lsp.setup {
-  init_options = {
-    settings = {
-      -- Any extra CLI arguments for `ruff` go here.
-      args = {},
-    }
-  }
-}
+-- Finalize Setup
+lsp.setup()
 
